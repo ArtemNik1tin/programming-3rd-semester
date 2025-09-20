406808ac07b99d53d10864e844f97d113296a497
ArtemNikit1n <st091174@student.spbu.ru>
2025-09-20 18:29:30 +0300

    feat: Add LazySingleThreaded + Tests

---
+++ Homeworks/02-lazy/Lazy.Tests/LazySingleThreadedTests.cs
@@ -0,0 +1,82 @@
+﻿// <copyright file="LazySingleThreadedTests.cs" company="ArtemNikit1n">
+// Licensed under the MIT License. See LICENSE file in the project root for full license information.
+// </copyright>
+
+namespace Lazy.Tests;
+
+#pragma warning disable CS1591
+#pragma warning disable SA1600
+public class LazySingleThreadedTests
+{
+    [Test]
+    public void Constructor_WithNullSupplier_ThrowsArgumentNullException()
+    {
+        Func<string> nullSupplier = null!;
+
+        Assert.Throws<ArgumentNullException>(() => new LazySingleThreaded<string>(nullSupplier));
+    }
+
+    [Test]
+    public void Get_OnFirstCall_ShouldReturnsCorrectValue()
+    {
+        var expected = "test value";
+        var lazy = new LazySingleThreaded<string>(() => expected);
+
+        var result = lazy.Get();
+
+        Assert.That(result, Is.EqualTo(expected));
+    }
+
+    [Test]
+    public void Get_OnSubsequentCalls_ShouldReturnsSameValue()
+    {
+        var callCount = 0;
+        var lazy = new LazySingleThreaded<int>(() =>
+        {
+            callCount++;
+            return 42;
+        });
+
+        var result1 = lazy.Get();
+        var result2 = lazy.Get();
+        var result3 = lazy.Get();
+
+        Assert.That(result1, Is.EqualTo(42));
+        Assert.That(result2, Is.EqualTo(42));
+        Assert.That(result3, Is.EqualTo(42));
+        Assert.That(callCount, Is.EqualTo(1), "Supplier should be called only once");
+    }
+
+    [Test]
+    public void Get_WhenSupplierReturnsNull_ShouldReturnsNull()
+    {
+        var lazy = new LazySingleThreaded<string?>(() => null);
+
+        var result1 = lazy.Get();
+        var result2 = lazy.Get();
+
+        Assert.That(result1, Is.Null);
+        Assert.That(result2, Is.Null);
+    }
+
+    [Test]
+    public void Get_WithValueTypes_ShouldWorksCorrectly()
+    {
+        var lazy = new LazySingleThreaded<int>(() => 123);
+
+        var result = lazy.Get();
+
+        Assert.That(result, Is.EqualTo(123));
+    }
+
+    [Test]
+    public void Get_WithReferenceTypes_ShouldReturnsSameInstance()
+    {
+        var expectedObject = new object();
+        var lazy = new LazySingleThreaded<object>(() => expectedObject);
+
+        var result = lazy.Get();
+
+        Assert.That(result, Is.SameAs(expectedObject));
+    }
+}
---
+++ Homeworks/02-lazy/Lazy/LazySingleThreaded.cs
@@ -0,0 +1,53 @@
+﻿// <copyright file="LazySingleThreaded.cs" company="ArtemNikit1n">
+// Licensed under the MIT License. See LICENSE file in the project root for full license information.
+// </copyright>
+
+namespace Lazy;
+
+/// <summary>
+/// Provides a simple, non-thread-safe implementation of the <see cref="ILazy{T}"/> interface.
+/// This implementation is intended for use in single-threaded scenarios only.
+/// </summary>
+/// <typeparam name="T">The type of the value that is being lazily initialized.</typeparam>
+/// <remarks>
+/// Initializes a new instance of the <see cref="LazySingleThreaded{T}"/> class.
+/// </remarks>
+/// <param name="supplier">The delegate that is invoked to produce the lazily initialized value when it is needed.</param>
+/// <exception cref="ArgumentNullException">Thrown when the <paramref name="supplier"/> is <see langword="null"/>.</exception>
+public class LazySingleThreaded<T>(Func<T> supplier) : ILazy<T>
+{
+    private Func<T>? supplier = supplier ?? throw new ArgumentNullException(nameof(supplier));
+    private T? value;
+    private bool isValueCreated;
+
+    /// <inheritdoc/>
+    /// <remarks>
+    /// If <see cref="Get"/> is called for the first time, this method will execute the supplier delegate,
+    /// store the result, and then return it. Subsequent calls will return the stored value without
+    /// re-executing the supplier.
+    /// </remarks>ы
+    /// <exception cref="InvalidOperationException">
+    /// Thrown if the supplier delegate was set to <see langword="null"/> internally before
+    /// the value could be initialized. This indicates a severe initialization error and should not
+    /// occur under normal circumstances.
+    /// </exception>
+    public T? Get()
+    {
+        if (!this.isValueCreated)
+        {
+            if (this.supplier is not null)
+            {
+                this.value = this.supplier();
+                this.isValueCreated = true;
+                this.supplier = null;
+            }
+            else
+            {
+                throw new InvalidOperationException(
+                    "Failed to initialize the lazy value. The supplier delegate is unavailable.");
+            }
+        }
+
+        return this.value;
+    }
+}

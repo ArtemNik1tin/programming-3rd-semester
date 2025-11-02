ae6b2ddb289b6231c49c2e0762ddcf76aad32424
ArtemNikit1n <st091174@student.spbu.ru>
2025-11-02 15:39:13 +0300

    feat: Add server interface

---
+++ Homeworks/04-simple-ftp/Client/Program.cs
@@ -0,0 +1,16 @@
+﻿// <copyright file="Program.cs" company="ArtemNikit1n">
+// Licensed under the MIT License. See LICENSE file in the project root for full license information.
+// </copyright>
+
+using System.Net.Sockets;
+
+const int port = 8888;
+using var client = new TcpClient("localhost", port);
+Console.WriteLine($"Sending \"Hello!\" to port {port}...");
+var stream = client.GetStream();
+var writer = new StreamWriter(stream);
+writer.WriteLine("Hello!");
+writer.Flush();
+var reader = new StreamReader(stream);
+var data = reader.ReadToEnd();
+Console.WriteLine($"Received: {data}");
---
+++ Homeworks/04-simple-ftp/Server/Server.cs
@@ -0,0 +1,50 @@
+﻿// <copyright file="Server.cs" company="ArtemNikit1n">
+// Licensed under the MIT License. See LICENSE file in the project root for full license information.
+// </copyright>
+
+namespace Server;
+
+using System.Net;
+using System.Net.Sockets;
+
+public class Server(int port) : IDisposable
+{
+    private readonly Lazy<TcpListener> listener = new(() => new TcpListener(IPAddress.Any, port));
+    private readonly CancellationTokenSource serverCts = new();
+    private readonly List<Task> currentTasks = [];
+
+    public async Task RunAsync()
+    {
+        ObjectDisposedException.ThrowIf(this.serverCts.Token.IsCancellationRequested, nameof(Server));
+
+        var listenerInstance = this.listener.Value;
+        listenerInstance.Start();
+        while (!this.serverCts.Token.IsCancellationRequested)
+        {
+            try
+            {
+                var socket = await listenerInstance.AcceptSocketAsync(this.serverCts.Token);
+                var task = Task.Run(() => Task.CompletedTask);
+                this.currentTasks.Add(task);
+            }
+            catch (OperationCanceledException)
+            {
+                break;
+            }
+        }
+    }
+
+    public void Dispose()
+    {
+        if (this.serverCts.Token.IsCancellationRequested || !this.listener.IsValueCreated)
+        {
+            return;
+        }
+
+        this.listener.Value.Stop();
+        this.serverCts.Cancel();
+        Task.WaitAll(this.currentTasks, this.serverCts.Token);
+        this.listener.Value.Dispose();
+        this.serverCts.Dispose();
+    }
+}
